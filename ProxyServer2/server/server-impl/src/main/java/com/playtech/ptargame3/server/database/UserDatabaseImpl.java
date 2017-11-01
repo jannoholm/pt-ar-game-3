6281cd24fec53a1de4ef7284ce4f9f62c428e10d
Janno Holm <janno.holm@playtech.com>
2017-11-01 17:41:44 +0200

    imrpoved web-api

--- ProxyServer2/server/server-impl/src/main/java/com/playtech/ptargame3/server/database/UserDatabaseImpl.java
+++ ProxyServer2/server/server-impl/src/main/java/com/playtech/ptargame3/server/database/UserDatabaseImpl.java
@@ -12,7 +12,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +34,7 @@ public class UserDatabaseImpl implements UserDatabase {
 
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<User> pendingWrites = new ArrayList<>();
+    private Map<Integer, User> userMap = new HashMap<>();
 
     public UserDatabaseImpl(DatabaseAccessImpl dbInit, ScheduledExecutorService executor) {
         this.dbInit = dbInit;
@@ -54,6 +57,25 @@ public class UserDatabaseImpl implements UserDatabase {
                             " NAME           TEXT    NOT NULL, " +
                             " EMAIL          TEXT    NOT NULL)";
             stmt.executeUpdate(sql);
+
+            // check if hidden field already exists
+            boolean alterNeeded = true;
+            sql = "PRAGMA table_info( " + TABLE_USERS + " )";
+            try (ResultSet result = stmt.executeQuery(sql)) {
+                while (result.next()) {
+                    if (result.getString("NAME").equals("HIDDEN")) {
+                        alterNeeded = false;
+                        break;
+                    }
+                }
+            }
+
+            // add hidden field. needed for backward compatibility
+            if (alterNeeded) {
+                sql = "ALTER TABLE " + TABLE_USERS + " ADD COLUMN HIDDEN INT NOT NULL DEFAULT 0";
+                stmt.executeUpdate(sql);
+            }
+
             logger.info("Users database created!");
         } catch (Exception e) {
             throw new SystemException("Unable to initialize users database", e);
@@ -65,14 +87,16 @@ public class UserDatabaseImpl implements UserDatabase {
     private void readTables() {
         Connection connection = dbInit.allocateConnection();
         try (Statement stmt = connection.createStatement()) {
-            String sql = "select ID, NAME, EMAIL from " + TABLE_USERS + " order by id";
+            String sql = "select ID, NAME, EMAIL, HIDDEN from " + TABLE_USERS + " order by id";
             ResultSet result = stmt.executeQuery(sql);
             while (result.next()) {
                 int id = result.getInt("ID");
                 String name = result.getString("NAME");
                 String email = result.getString("EMAIL");
-                User user = new User(id, name.toUpperCase(), email);
+                int hidden = result.getInt("HIDDEN");
+                User user = new User(id, name.toUpperCase(), email, hidden > 0);
                 users.add(user);
+                userMap.put(user.getId(), user);
                 logger.info("User read from database: " + user);
                 if (idGenerator.get() < id) {
                     idGenerator.set(id);
@@ -94,13 +118,14 @@ public class UserDatabaseImpl implements UserDatabase {
                     todo.addAll(pendingWrites);
                 }
                 if (todo.size() > 0) {
-                    String insertSql = "insert into " + TABLE_USERS + " (ID, NAME, EMAIL) VALUES (?, ?, ?)";
+                    String insertSql = "insert into " + TABLE_USERS + " (ID, NAME, EMAIL, HIDDEN) VALUES (?, ?, ?, ?)";
                     Connection connection = dbInit.allocateConnection();
                     try (PreparedStatement stmt = connection.prepareStatement(insertSql)) {
                         for (User user : todo) {
                             stmt.setInt(1, user.getId());
                             stmt.setString(2, user.getName());
                             stmt.setString(3, user.getEmail());
+                            stmt.setInt(4, user.isHidden() ? 1 : 0);
                             stmt.executeUpdate();
                             logger.info("User written to database: " + user);
                             synchronized (this) {
@@ -119,12 +144,31 @@ public class UserDatabaseImpl implements UserDatabase {
         }
     }
 
-    public void addUser(String name, String email) {
+    public User addUser(String name, String email) {
         if (StringUtil.isNull(name)) throw new NullPointerException("Name cannot be null.");
-        User user = new User(idGenerator.incrementAndGet(), name.toUpperCase(), email);
+        User user = new User(idGenerator.incrementAndGet(), name.toUpperCase(), email, false);
         synchronized (this) {
             pendingWrites.add(user);
             users.add(user);
+            userMap.put(user.getId(), user);
+        }
+        return user;
+    }
+
+    @Override
+    public User getUser(int id) {
+        synchronized (this) {
+            return userMap.get(id);
+        }
+    }
+
+    @Override
+    public void updateUser(User user) {
+        synchronized (this) {
+            User existing = userMap.get(user.getId());
+            if (existing != null) {
+                userMap.put(user.getId(), user);
+            }
         }
     }
 
