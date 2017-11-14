d556933d3f41315b2b7a73e8124c15c0567684fe
Martin Kiilo <martin.kiilo@gmail.com>
2017-11-14 15:47:29 +0000

    Changed filename for leaderboard cache

--- ProxyServer2/run/html/store_leaderboard.php
+++ ProxyServer2/run/html/store_leaderboard.php
@@ -1,8 +1,7 @@
-Result: <?php
-
+<?php
 if ($_POST['data'])
 {
-  $file_handle = fopen('leaderboard_1', 'w');
+  $file_handle = fopen('leaderboard', 'w');
   fwrite($file_handle, $_POST['data']);
   fclose($file_handle);
   echo 'OK';
